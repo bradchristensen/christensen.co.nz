08defd671660afc93d5d39a1d840aa7ffc7e13ae
Brad Christensen <brad@christensen.co.nz>
2014-05-23 02:12:39 +1200

    Forget about my amazing plans to populate the site with content - looks like I'm not going to be doing any more freelancing - let's just condense it down into a little blurb about me with a link to LinkedIn :)

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-$('document').ready(function(){
-	resize();
-
+$(document).ready(function(){
 	$('a').click(function(){
 		if (this.hash.length > 1) {
 			$.scrollTo (this.hash, 500);
@@ -8,29 +6,16 @@ $('document').ready(function(){
 		}
 	});
 
-	$(".portfolio a").fancybox({
-		openEffect: 'elastic',
-		closeEffect: 'elastic'
-	});
-
-	$(window).resize(resize);
-
-	$(window).scroll(function () {
+	$(window).scroll(function() {
 		var st = $(document).scrollTop();
 
-		var i = (st + $(window).height() / 3) / $(window).height();
-		$('#map li').removeClass('current');
-		$('#map li').eq(i).addClass('current');
-
-		$('.slide').each(function() {
-			var offset = (st-$(this).offset().top)/10;
+		$('.slide').each(function(i) {
+			var offset = st - $(this).offset().top;
 			
-			$(this).css('backgroundPosition', 'center ' + offset + 'px');
-			$('.wrapper', this).css('opacity', 1.2-(offset/100));
-		})
+			if ( offset + $(window).height()/2 > 0 ) {
+				$('#map li').removeClass('current');
+				$('#map li').eq(i).addClass('current');
+			}
+		});
 	});
 });
-
-var resize = function() {
-	$('.slide').css('height', '' + $(window).height() + 'px');
-}
