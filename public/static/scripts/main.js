5171f9b21d6dd220b464bf7c4d71001f4ac18c50
Brad Christensen <brad@christensen.co.nz>
2015-07-18 00:36:33 +1200

    Use Babel + some fancy concatenating to replace the slow rjs optimiser, and bundle in Almond instead of RequireJS

--- public/static/scripts/main.js
+++ public/static/scripts/main.js
@@ -1,23 +1,23 @@
-require(['vendor/smooth-scroll'], function (smoothScroll) {
-	smoothScroll.init();
+import smoothScroll from 'vendor/smooth-scroll';
 
-	var mapListItems = document.getElementById('map').children;
-	var slides = document.getElementsByClassName('slide');
-	window.addEventListener('scroll', function () {
-		var currentSlide = -1;
-		Array.prototype.forEach.call(slides, function (slide, i) {
-			var bottom = slide.getBoundingClientRect().bottom;
+smoothScroll.init();
 
-			if (currentSlide < 0 && bottom > window.innerHeight / 2) {
-				currentSlide = i;
-			}
-		});
+var mapListItems = document.getElementById('map').children;
+var slides = document.getElementsByClassName('slide');
+window.addEventListener('scroll', () => {
+	var currentSlide = -1;
+	Array.prototype.forEach.call(slides, (slide, i) => {
+		var bottom = slide.getBoundingClientRect().bottom;
 
-		if (currentSlide >= 0) {
-			Array.prototype.forEach.call(mapListItems, function (li) {
-				li.classList.remove('current');
-			});
-			mapListItems[currentSlide].classList.add('current');
+		if (currentSlide < 0 && bottom > window.innerHeight / 2) {
+			currentSlide = i;
 		}
 	});
+
+	if (currentSlide >= 0) {
+		Array.prototype.forEach.call(mapListItems, li => {
+			li.classList.remove('current');
+		});
+		mapListItems[currentSlide].classList.add('current');
+	}
 });
