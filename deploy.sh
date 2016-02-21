19b1829b2a0e0eda6d52f5ea65ac6b551321348b
Brad Christensen <brad@christensen.co.nz>
2016-02-21 17:42:18 +1300

    Update travis deployment config

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ mkdir build
 mv * build
 tar -czf christensen_co_nz.tgz build
 export SSHPASS=$DEPLOY_PASS
-sshpass -e scp christensen_co_nz.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e scp christensen_co_nz.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/packages
 sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/scripts/deployChristensen.sh
